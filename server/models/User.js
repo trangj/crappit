8d354b5c50ff55afc82cae0cb5ab91d67f33a7c0
trangj <joshua.trang@hotmail.com>
2021-05-18 13:22:48 -0400

    added delete end point for moderation and added button for delete moderation

--- server/models/User.js
+++ server/models/User.js
@@ -2,26 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  followedTopics: [String],
-  register_date: {
-    type: Date,
-    default: Date.now,
-  },
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
+	username: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	followedTopics: [String],
+	topicsModerating: [String],
+	register_date: {
+		type: Date,
+		default: Date.now,
+	},
+	resetPasswordToken: String,
+	resetPasswordExpires: Date,
 });
 
 module.exports = User = mongoose.model("User", userSchema);
--- server/routes/moderation.js
+++ server/routes/moderation.js
@@ -8,11 +8,11 @@ const auth = require("../middleware/auth");
 const Topic = require("../models/Topic");
 const User = require("../models/User");
 
-// @route   POST /api/moderation/:topic
+// @route   POST /api/moderation/t/:topic
 // @desc    Add a moderator to a topic
 // @access  Public
 
-router.post("/:topic", auth, async (req, res) => {
+router.post("/t/:topic", auth, async (req, res) => {
 	try {
 		const user = await User.findOne({ username: req.body.username });
 		if (!user) throw Error("User does not exist");
@@ -27,7 +27,9 @@ router.post("/:topic", auth, async (req, res) => {
 			).length
 		)
 			throw Error("User is already a moderator");
+		user.topicsModerating.push(topic.title);
 		topic.moderators.push(user);
+		await user.save();
 		await topic.save();
 		res.status(200).json({
 			topic,
@@ -37,6 +39,40 @@ router.post("/:topic", auth, async (req, res) => {
 		res.status(400).json({
 			status: { text: err.message, severity: "error" },
 		});
+	}
+});
+
+router.delete("/p/:post", auth, async (req, res) => {
+	try {
+		const post = await Post.findOne({ _id: req.params.post });
+		if (!post) throw Error("Post does not exist");
+		const topic = await Topic.findOne({ title: post.topic }).populate({
+			path: "moderators",
+			select: "username",
+		});
+		if (!topic) throw Error("Topic does not exist");
+		if (
+			!!!topic.moderators.filter(
+				(moderator) => moderator.username === req.user.username
+			).length
+		)
+			throw Error("You are not a moderator for this topic");
+
+		const query = await Post.deleteOne({
+			_id: req.params.id,
+		});
+		if (!query.deletedCount)
+			throw Error("Post does not exist or you are not the author");
+		await Comment.deleteMany({ post: req.params.id });
+		await Topic.updateOne(
+			{ title: req.params.topic },
+			{ $pull: { posts: req.params.id } }
+		);
+		res.status(200).json({
+			status: { text: "Post successfully deleted", severity: "success" },
+		});
+	} catch (err) {
+		res.status(400).json({ status: { text: err.message, severity: "error" } });
 	}
 });
 
