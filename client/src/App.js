4174f7a9f6995c0849db48c9ed45081ce7d68340
trangj <joshua.trang@hotmail.com>
2021-05-19 22:09:20 -0400

    prevent refetching on window refocus

--- client/src/App.js
+++ client/src/App.js
@@ -21,9 +21,17 @@ import theme from "./theme";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
+
 function App() {
 	return (
-		<QueryClientProvider client={new QueryClient()}>
+		<QueryClientProvider client={queryClient}>
 			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 			<UserProvider>
 				<ChakraProvider theme={theme}>
