76e40de7f8183ec4f9ad48f9504a11c9d1388533
trangj <joshua.trang@hotmail.com>
2021-06-06 13:00:07 -0400

    typeorm gonna make me freak

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -3,7 +3,7 @@ const { SnakeNamingStrategy } = require("typeorm-naming-strategies");
 module.exports = {
 	type: "postgres",
 	entities: ["dist/entities/**/*.js"],
-	migrations: ["dist/migration/**/*.js"],
+	migrations: ["dist/migrations/**/*.js"],
 	url: process.env.DATABASE_URL,
 	namingStrategy: new SnakeNamingStrategy(),
 	ssl: {
