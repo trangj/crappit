963bb612b1a40881e6d527a1ce20a60d80dc177c
trangj <joshua.trang@hotmail.com>
2021-08-12 22:38:32 -0400

    redesigned all topics page

--- client/src/pages/t/index.tsx
+++ client/src/pages/t/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import useTopics, { fetchTopics } from "../../hooks/topic-query/useTopics";
-import { LinkCard, LinkCardOverlay } from "../../ui/LinkCard";
-import { Container } from "../../ui/Container";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { QueryClient } from "react-query";
 import { dehydrate } from "react-query/hydration";
 import PostSkeleton from "src/components/util/PostSkeleton";
+import { Card } from "src/ui/Card";
+import Image from 'next/image';
+import { Avatar } from "src/ui/Avatar";
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 	const queryClient = new QueryClient();
@@ -22,32 +23,46 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 const AllTopics = () => {
 	const { isLoading, data: topics } = useTopics();
 
+	if (isLoading) {
+		return (
+			<>
+				<PostSkeleton />
+				<PostSkeleton />
+			</>
+		);
+	}
+
 	return (
-		<Container>
+		<>
 			<Head>
 				<title>Discover Topics</title>
 				<meta name="description" content="View today's top growing Reddit communities. Filter by category to view top communities in sports, gaming, television and more." />
 			</Head>
-			{!isLoading ? (
-				topics?.map((topic) => (
-					<LinkCard key={topic.title} className="p-2">
-						<h6>
-							<Link passHref href={`t/${topic.title}`}>
-								<LinkCardOverlay>
-									t/{topic.title}
-								</LinkCardOverlay>
-							</Link>
-						</h6>
-						<div>{topic.description}</div>
-					</LinkCard>
-				))
-			) : (
-				<>
-					<PostSkeleton />
-					<PostSkeleton />
-				</>
-			)}
-		</Container>
+			<div className="bg-white dark:bg-gray-850 h-24 mt-12 flex items-center">
+				<div className="container mx-auto max-w-5xl sm:px-5">
+					<h6>
+						Discover Topics
+					</h6>
+					<small className="text-gray-500 dark:text-gray-400">
+						Browse all the topics that Crappit has to offer.
+					</small>
+				</div>
+			</div>
+			<div className="mt-4 container mx-auto max-w-5xl sm:px-5">
+				<Card>
+					{topics?.map((topic, i) => (
+						<Link key={topic.title} passHref href={`t/${topic.title}`}>
+							<a className={`flex items-center gap-2 p-3 border-gray-300 dark:border-gray-700 ${i === 0 ? '' : 'border-t'}`}>
+								<div className=" h-11 w-11 rounded-full">
+									{!topic.icon_image_name ? <Avatar /> : <Image alt="topic icon" src={topic.icon_image_name} width={44} height={44} className="rounded-full" />}
+								</div>
+								<div className="font-medium">t/{topic.title}</div>
+							</a>
+						</Link>
+					))}
+				</Card>
+			</div>
+		</>
 	);
 };
 
