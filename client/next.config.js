0a775aea4a2225e3284fca7d60c2bfd65a54ea29
trangj <joshua.trang@hotmail.com>
2021-12-15 23:34:06 -0500

    nextjs font optimization is broken

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	reactStrictMode: true,
+	optimizeFonts: false,
 	images: {
 		loader: "imgix",
 		path: "https://crappit.imgix.net/",
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -8,7 +8,9 @@ export default class MyDocument extends Document {
                         __html:
                             `!function(){var e=window.localStorage.getItem("theme"),t=window.document.documentElement;e?t.classList.add(e):(e=window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light")&&t.classList.add(e)}();`
                     }} />
-                    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
                     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
                     <link rel="apple-touch-icon" href="/img/logo.png" />
                     <link rel="apple-touch-startup-image" href="/img/logo512.png" />
